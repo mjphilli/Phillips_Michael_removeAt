9de339e634f3b6de837838ae551d9e39294b269e
mjphilli <phillipsjmichael@gmail.com>
2023-03-10 22:49:38 -0600

    First commit. Just comments.

---
+++ Phillips_Michael_removeAt.js
@@ -0,0 +1,5 @@
+// Given an array and an index, remove and return the array value at that index.
+// Do this without using built-in array methods except for pop(). Think of popFront(arr) as equivalent to removeAt(arr,0).
+
+// For example, removeAt([5, 93, 22, 4], 2) should return 22, and the original array should result as [5, 93, 4].
+
